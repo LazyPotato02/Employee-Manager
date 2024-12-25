979f8c6fd5f79d03ff1bed8523c4ba189e7ce04f
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-12-25 23:14:04 +0200

    Added serializers and url files for cells and altered models so it works with one to many for employees to cells

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from cells.models import Cell
+
+
 # Create your models here.
 
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-
+    cell = models.ForeignKey(Cell, on_delete=models.CASCADE, related_name="employees")
     def str(self):
         return f'{self.first_name} {self.last_name}'
