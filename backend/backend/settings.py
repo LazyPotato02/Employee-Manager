e30613fabbe82f77a6a9822999dc798efd5b7612
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-12-25 15:36:24 +0200

    Created orders model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'employees',
+    'orders',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Orders(models.Model):
+    order_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    done_quantity = models.IntegerField(default=0)
