59ed79b52f708875aa85d1cd5cd65ca083cb498a
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-01-03 15:08:16 +0200

    Created functionality for login component

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../services/auth/auth';
+import {Router} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf,
+
+  ],
   templateUrl: './login.component.html',
+  standalone: true,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  errorMessage: string | null = null;
+  loginForm: FormGroup;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (response) => {
+          if (response.message === 'Login successful') {
+            this.errorMessage = null;
+            this.router.navigate(['/']);
+          }
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+          if (error.status === 401) {
+            this.errorMessage = 'Invalid username or password.';
+          } else {
+            this.errorMessage = 'An unexpected error occurred. Please try again later.';
+          }
+        }
+      });
+    }
+  }
 }
