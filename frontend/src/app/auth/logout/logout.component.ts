e7b9af389005a1a6c9ae6f1bff25641c9a694fc9
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-01-03 15:08:33 +0200

    Created functionality for logout component

---
+++ frontend/src/app/auth/logout/logout.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../services/auth/auth';
+
+@Component({
+  selector: 'app-logout',
+  template: '',
+  standalone: true
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.authService.logout().add(() => {
+      this.router.navigate(['/']);
+    });
+  }
+}
