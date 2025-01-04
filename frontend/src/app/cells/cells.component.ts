42bd75be6c2c74f873a2848750593d5bd3ac19a3
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-01-04 14:27:12 +0200

    Added retrieve cell id from url

--- frontend/src/app/cells/cells.component.ts
+++ frontend/src/app/cells/cells.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CellServices} from '../services/cells/cell.services';
 
 @Component({
   selector: 'app-cells',
@@ -8,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './cells.component.css'
 })
 export class CellsComponent {
+  id: string | undefined;
 
+  constructor(private router: Router, private cellService: CellServices, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+  }
 }
