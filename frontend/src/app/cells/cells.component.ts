bb8d85270c9767b0c787ee097d088222e9677071
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-01-04 13:55:12 +0200

    Added cell services for creating cell

---
+++ frontend/src/app/cells/cells.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cells',
+  imports: [],
+  templateUrl: './cells.component.html',
+  standalone: true,
+  styleUrl: './cells.component.css'
+})
+export class CellsComponent {
+
+}
